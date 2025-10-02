b7bf52b616db9d7d5315ae359c0a8083dd70ab45
egarrisxn <egarrisxn@gmail.com>
2025-10-02 12:36:19 -0400

    cleaning up some stuff

--- app/global-not-found.tsx
+++ app/global-not-found.tsx
@@ -22,7 +22,7 @@ export default function GlobalNotFound() {
         <div className='mx-auto grid min-h-dvh w-full place-items-center p-6 md:p-10'>
           <div className='w-full max-w-md'>
             <div className='flex flex-col gap-6 rounded-xl border border-b-2 border-border border-b-border/90 bg-card py-6 text-card-foreground shadow-2xl'>
-              <div className='grid auto-rows-min grid-rows-[auto_auto] items-start gap-1.5 px-6 has-data-[slot=card-action]:grid-cols-[1fr_auto] [.border-b]:pb-6'>
+              <div className='grid auto-rows-min grid-rows-[auto_auto] items-start gap-1.5 px-6'>
                 <h1 className='text-2xl leading-none font-semibold'>
                   Page Not Found.
                 </h1>
@@ -32,7 +32,7 @@ export default function GlobalNotFound() {
                   Could not find requested resource.
                 </p>
               </div>
-              <div className='flex items-center justify-end px-6 [.border-t]:pt-6'>
+              <div className='flex items-center justify-end px-6'>
                 <Link
                   className='cursor-pointer text-sm font-medium text-link underline-offset-4 transition-all hover:text-link/80 hover:underline'
                   href='/'
