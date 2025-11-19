bf852f9813bb0ff2f445761831b455ed503bd8c7
egarrisxn <egarrisxn@gmail.com>
2025-11-19 15:49:36 -0500

    updates

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -10,7 +10,7 @@ export async function updateSession(request: NextRequest) {
 
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
     {
       cookies: {
         getAll() {
@@ -31,9 +31,9 @@ export async function updateSession(request: NextRequest) {
     }
   );
 
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
+  const { data } = await supabase.auth.getClaims();
+
+  const user = data?.claims;
 
   const pathname = request.nextUrl.pathname;
 
